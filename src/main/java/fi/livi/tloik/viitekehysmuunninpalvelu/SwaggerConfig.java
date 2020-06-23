1ab3505881f222f273391683c234e5235382a9ba
Hoai Truong <hoai.truong@sitowise.com>
2020-06-23 12:42:23 +0300

    contact added

--- src/main/java/fi/livi/tloik/viitekehysmuunninpalvelu/SwaggerConfig.java
+++ src/main/java/fi/livi/tloik/viitekehysmuunninpalvelu/SwaggerConfig.java
@@ -11,9 +11,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	private String contactOperator = "ptptuki@sitowise.com";
 	
     @Bean
     public Docket productApi() {
@@ -30,6 +34,7 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("VIITEKEHYSMUUNNIN")
                 .version("uusi")
+                .description("Tuki: " + contactOperator )
                 .build();
         }
 
