586907a260032375cdc6be5a0ff3ecc3b256b130
Jyrki Jylhä <jyrki.jylha@sitowise.com>
2020-12-15 12:50:17 +0200

    Changed the version number

--- src/main/java/fi/livi/tloik/viitekehysmuunninpalvelu/controller/ViitekehysmuunninPalveluController.java
+++ src/main/java/fi/livi/tloik/viitekehysmuunninpalvelu/controller/ViitekehysmuunninPalveluController.java
@@ -49,7 +49,7 @@ public class ViitekehysmuunninPalveluController {
     @Autowired
     private Environment env;
     
-    public static final String API_VERSION = "3.0.3";
+    public static final String API_VERSION = "1.0.0";
     
     
     @RequestMapping(value = "versio", method = RequestMethod.GET)
