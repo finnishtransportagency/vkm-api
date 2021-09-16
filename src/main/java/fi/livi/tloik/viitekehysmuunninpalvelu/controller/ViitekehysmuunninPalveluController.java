8c87371dadd691524e373d83d30d5c725c39c156
Jyrki Jylhä <jyrki.jylha@sitowise.com>
2021-09-16 14:03:04 +0300

    Changed version number

--- src/main/java/fi/livi/tloik/viitekehysmuunninpalvelu/controller/ViitekehysmuunninPalveluController.java
+++ src/main/java/fi/livi/tloik/viitekehysmuunninpalvelu/controller/ViitekehysmuunninPalveluController.java
@@ -65,7 +65,7 @@ public class ViitekehysmuunninPalveluController {
     
     private boolean addMetadata = false;
     
-    public static final String API_VERSION = "4.1.4";
+    public static final String API_VERSION = "4.1.5";
     
     // Comment for build
     
